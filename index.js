f62854a4d73339498e070f668cb7c5d7310c8c59
bubkoo <bubkoo.wy@gmail.com>
2016-04-26 20:32:57 +0800

    v1.0.2

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 'use strict';
 
-module.exports = function () {
-  var color = Math.floor(Math.random() * (16 * 16 * 16 * 16 * 16 * 16 - 1)).toString(16);
-  return '#' + ('000000' + color).slice(-6);
+var color = require('color');
+
+var ratio = 0.618033988749895;
+var hue   = Math.random();
+
+
+module.exports = function (options) {
+
+  hue += ratio;
+  hue %= 1;
+
+  options = options || {};
+
+  var saturation = options.saturation;
+  var value      = options.value;
+
+  if (typeof saturation !== 'number') {
+    saturation = 0.5;
+  }
+
+  if (typeof value !== 'number') {
+    value = 0.95;
+  }
+
+  var hsv = {
+    h: hue * 360,
+    s: saturation * 100,
+    v: value * 100
+  };
+
+  return options.raw ? hsv : color(hsv);
 };
